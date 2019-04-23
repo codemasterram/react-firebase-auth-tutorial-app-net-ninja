a6e478e14b9922fed171b1137f4e0c1019b6c085
ramsapkota <iamramsapkota@gmail.com>
2019-04-23 23:01:34 +0545

    Dashboard component that nests notification and projectlist components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import NavBar from './layout/Navbar';
+import Dashboard from './dashboard/Dashboard';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <NavBar />
+        <Switch>
+          <Route path='/' component={Dashboard}></Route>
+        </Switch>
       </div>
     </BrowserRouter>
   );
