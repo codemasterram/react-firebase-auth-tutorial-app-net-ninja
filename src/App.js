eae5b1f6a66b5e5c9c0601e37e684c20ef92bd01
ramsapkota <iamramsapkota@gmail.com>
2019-04-23 23:48:17 +0545

    signin and signup component with basic state and change done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import NavBar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
 import ProjectDetails from './components/projects/ProjectDetails'
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
         <Switch>
           <Route exact path='/' component={Dashboard}></Route>
           <Route exact path='/project/:id' component={ProjectDetails}></Route>
+          <Route exact path='/signin' component={SignIn}></Route>
+          <Route exact path='/signup' component={SignUp}></Route>
         </Switch>
       </div>
     </BrowserRouter>
